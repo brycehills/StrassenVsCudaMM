0c4af358d986e3e5efa042d50e73a8fd4d505ea5
Bryce Hills <bhills@bender.engr.ucr.edu>
2021-10-29 15:23:03 -0700

    added load + boundary checking to kernel mm

--- kernel.cu
+++ kernel.cu
@@ -17,7 +17,48 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
 
     /*************************************************************************/
     // INSERT KERNEL CODE HERE
-        
+    //declare shared memory/thread ints
+    __shared__ float ds_M[TILE_SIZE][TILE_SIZE];
+    __shared__ float ds_N[TILE_SIZE][TILE_SIZE];
+
+
+    //declare matrix index vars
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    ///define row/col
+    int Row = by * blockDim.y + ty;
+    int Col = bx * blockDim.x + tx;
+
+    
+    // load data from A,B into shared mem + boundary checking & zero padding
+    for(int p = 0; p < (k-1)/TILE_SIZE-1;p++)
+    {
+    	if(Row < m && (p * TILE_SIZE + tx < k)) //load M - within boundary;  note: (a = m x k)
+	{
+	    ds_M[ty][tx] = A[Row * k + p * TILE_SIZE + tx];
+	}
+	else // pad 0
+	{
+	   ds_M[ty][tx] = 0.0;
+	}
+	if((p*TILE_SIZE + ty < k) && Col < n) // load N - within boundary; note: (b = k x n)
+	{
+	   ds_N[ty][tx] = B[(p*TILE_SIZE + ty) * k + Col];
+	}
+	else // pad 0
+	{
+	    ds_N[ty][tx] = 0.0;
+	}
+		
+    }
+
+    //perform computation (inner product) into p variable
+
+    //sync 
+    // loop to load p variable into C    
     /*************************************************************************/
 }
 
