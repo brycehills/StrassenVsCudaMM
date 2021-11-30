828d3bd5ca77eb34b0e0fc192b3bbdc9e6c27512
Bryce Hills <bhills@bender.engr.ucr.edu>
2021-11-30 13:31:51 -0800

    added naive mm

--- mm.cpp
+++ mm.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 #define MM_SIZE 1000
 
-void mm(float a[][MM_SIZE], float b[][MM_SIZE], int a_row_size, int a_col_size, int b_row_size, int b_col_size){
+void mm(float a[MM_SIZE][MM_SIZE], float b[MM_SIZE][MM_SIZE]){
 
-	int r[a_row_size][b_col_size];
+	float r[MM_SIZE][MM_SIZE];
 	cout << "starting normal mult..." << endl;
 
-	for(int i = 0; i < a_row_size; i++){
-		for(int j = 0; j < a_col_size; j++){
+	for(int i = 0; i < MM_SIZE; i++){
+		for(int j = 0; j < MM_SIZE; j++){
 			r[i][j] = 0;
-			for(int k = 0; k < b_row_size; k++){
+			for(int k = 0; k < MM_SIZE; k++){
 				r[i][j] += a[i][k] * b[k][j];
 			}
-		cout << r[i][j] << "\t";
+		cout << r[i][j] << "     ";
 		}
 	cout << endl;
 	}
